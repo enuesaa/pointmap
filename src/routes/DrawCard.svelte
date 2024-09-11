2d9aed1b87ce157a18944ed03ed982b87aa0589b
nua <enuesaa@gmail.com>
2024-09-11 19:31:53 +0900

    feat(ui): styling of create-new-drawer-link

--- src/routes/DrawCard.svelte
+++ src/routes/DrawCard.svelte
@@ -2,6 +2,12 @@
 	export let drawingId: string
 </script>
 
-<a href="/drawer" class="w-[32%] aspect-video border-black border-solid border-2 block">
+<a href="/drawer/{drawingId}">
 	{drawingId}
 </a>
+
+<style lang="postcss">
+	a {
+		@apply w-[32%] aspect-video border-black border-solid border-2 block;
+	}
+</style>
--- src/routes/DrawCardNewLink.svelte
+++ src/routes/DrawCardNewLink.svelte
@@ -1,3 +1,10 @@
-<a href="/drawer" class="w-[32%] aspect-video border-black border-solid border-2 block">
-	drawing
+<a href="/drawer">
+	+
 </a>
+
+<style lang="postcss">
+	a {
+		@apply w-[32%] aspect-video border-black border-solid border-2 block;
+		@apply text-5xl flex items-center justify-center;
+	}
+</style>
