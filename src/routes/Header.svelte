a36f5b896b49b6e5d45b3ea0560cce56d0a165c2
nua <enuesaa@gmail.com>
2024-09-11 19:08:04 +0900

    fix: failed to load css file

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,3 +1,7 @@
-<header>
-	<h1>pointmap</h1>
+<header class="container mx-auto pt-2 pb-4">
+	<a href="/">
+		<h1 class="font-zenkaku text-2xl font-bold px-3">
+			pointmap
+		</h1>
+	</a>
 </header>
