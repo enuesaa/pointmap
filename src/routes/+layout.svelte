8a2eaa521096d06be799d851ae9872f92416e3b7
nua <enuesaa@gmail.com>
2024-09-14 19:53:23 +0900

    feat(ui): add work-in-progress banner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import './app.css'
 	import Header from './Header.svelte'
+	import WorkInProgressBanner from './WorkInProgressBanner.svelte'
 </script>
 
+<WorkInProgressBanner />
 <Header />
 
 <main class="mx-auto container w-10/12">
---
+++ src/routes/WorkInProgressBanner.svelte
@@ -0,0 +1,3 @@
+<section class="py-2 px-10 text-center text-grayer bg-black select-none">
+	Experimental Website. Work in progress..
+</section>
