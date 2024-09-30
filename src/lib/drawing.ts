ca55974e26a2cc4284d98cd9d27308cb16debd00
nua <enuesaa@gmail.com>
2024-09-30 18:58:40 +0900

    fix(registry): missing property svgOnMouseDown

--- src/lib/drawing.ts
+++ src/lib/drawing.ts
@@ -10,6 +10,7 @@ export const createRegistry = (): Registry => {
 		svgOnMouseMove: undefined,
 		svgOnMouseUp: undefined,
 		svgOnMouseLeave: undefined,
+		svgOnMouseDown: undefined,
 		rectOnMouseDown: undefined,
 	}
 }
